c5a60b45fa05fd720d85dd8f9eb20ea0a283dae8
Nivan Gujral <nivangujral@nitins-macbook-pro.local>
2018-09-09 22:18:27 -0400

    Convert to MVC design pattern.

---
+++ Todoey/Data model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Todoey
+//
+//  Created by Nivan Gujral on 9/8/18.
+//  Copyright © 2018 Nivan Gujral. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
